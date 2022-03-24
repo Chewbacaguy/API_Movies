0fef5448162c53e96cb090be333a76297c49814d
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-03-24 12:31:52 -0600

    agruegue los files buenos

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <v-app-bar app color="#3A0453" dark>
+      <v-icon class="mr-2">fas fa-video</v-icon>
+      <v-toolbar-title
+        ><v-btn text class="ml-2" color="button-color" to="/dashboard">
+          Netflix Statz
+        </v-btn></v-toolbar-title
+      >
+    </v-app-bar>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Toolbar",
+};
+</script>
+
+<style lang="scss">
+.toolbar {
+  margin: 0 100px;
+}
+</style>
---
+++ src/plugins/index.js
@@ -0,0 +1,4 @@
+import './axios'
+import './firebase'
+import './db'
+import 'vuefire'
