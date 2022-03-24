20eae20e0c90a9206b49efec1c5b1088bdb44549
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2022-03-24 12:27:38 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app style="background: none !important">
+    <Toolbar />
+    <v-main>
+      <v-container fluid fill-height>
+        <router-view />
+      </v-container>
+    </v-main>
+    <!-- <Footer /> -->
+  </v-app>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+// import NavBar from "@/components/NavBar.vue";
+import Toolbar from "@/components/Toolbar.vue";
+// import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    // NavBar,
+    Toolbar,
+    // Footer,
+  },
+
+  computed: {
+    ...mapGetters(["currentUser"]),
+  },
+
+  watch: {
+    currentUser(currentUser) {
+      if (currentUser) {
+        this.$router.push("/dashboard");
+      }
+    },
+  },
+};
+</script>
